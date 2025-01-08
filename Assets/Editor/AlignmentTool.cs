81a718313f3e634603f77a1efbd3cbe3183d7592
Sanzio <sanzio.pedersoli@gmail.com>
2025-01-09 00:03:11 +0100

    Ui and alinment tools, Menu manager

---
+++ Assets/Editor/AlignmentTool.cs
@@ -0,0 +1,84 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class AlignmentTool : EditorWindow
+{
+    [MenuItem("Tools/Alignment Tool")]
+    public static void ShowWindow()
+    {
+        GetWindow<AlignmentTool>("Alignment Tool");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Alignment Options", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Align Vertically"))
+        {
+            AlignObjects(Axis.Vertical);
+        }
+
+        if (GUILayout.Button("Align Horizontally"))
+        {
+            AlignObjects(Axis.Horizontal);
+        }
+
+        if (GUILayout.Button("Distribute Regularly"))
+        {
+            DistributeObjects();
+        }
+    }
+
+    private enum Axis
+    {
+        Vertical,
+        Horizontal
+    }
+
+    private static void AlignObjects(Axis axis)
+    {
+        var selectedObjects = Selection.transforms;
+
+        if (selectedObjects.Length < 2)
+        {
+            Debug.LogWarning("Select at least two objects to align.");
+            return;
+        }
+
+        float center = 0f;
+        foreach (var obj in selectedObjects)
+        {
+            center += axis == Axis.Vertical ? obj.position.y : obj.position.x;
+        }
+        center /= selectedObjects.Length;
+
+        foreach (var obj in selectedObjects)
+        {
+            Undo.RecordObject(obj, "Align Objects");
+            obj.position = axis == Axis.Vertical
+                ? new Vector3(obj.position.x, center, obj.position.z)
+                : new Vector3(center, obj.position.y, obj.position.z);
+        }
+    }
+
+    private static void DistributeObjects()
+    {
+        var selectedObjects = Selection.transforms;
+
+        if (selectedObjects.Length < 3)
+        {
+            Debug.LogWarning("Select at least three objects to distribute.");
+            return;
+        }
+
+        var startPosition = selectedObjects[0].transform.position;
+        var targetPosition = selectedObjects[^1].transform.position;
+        var floatlenght = selectedObjects.Length - 1f;
+
+        for (int i = 1; i < selectedObjects.Length - 1; i++)
+        {
+            Undo.RecordObject(selectedObjects[i], "Distribute Objects");
+            selectedObjects[i].position = Vector3.Lerp(startPosition, targetPosition, i / floatlenght);
+        }
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,53 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MenuManager : MonoBehaviour
+{
+    [Serializable] private struct SceneLoadButton
+    {
+        public string SceneName;
+        public Button Button;
+        public float TransitionTime;
+    }
+
+    [SerializeField] private List<SceneLoadButton> _sceneLoadButtons;
+    [SerializeField] private Button _closeButton;
+
+    private void Awake()
+    {
+        foreach (var sceneLoadButton in _sceneLoadButtons)
+            sceneLoadButton.Button.onClick.AddListener(async () =>
+            {
+                SetButtonsActive(false);
+                await Timer(sceneLoadButton.TransitionTime, () => LoadScene(sceneLoadButton.SceneName));
+                SetButtonsActive(true);
+            });
+
+        if (_closeButton) _closeButton.onClick.AddListener(() =>
+        {
+            SetButtonsActive(false);
+            Application.Quit();
+        });
+    }
+
+    public void SetButtonsActive(bool areActive)
+    {
+        foreach (SceneLoadButton sceneLoadButton in _sceneLoadButtons) sceneLoadButton.Button.interactable = areActive;
+    }
+
+    private async UniTask Timer(float transitionTime, Action action)
+    {
+        await UniTask.WaitForSeconds(transitionTime);
+        action.Invoke();
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
